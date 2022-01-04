6a17493f0b688d5b49c168a71e185e7b22affef5
zemca <79542474+zemca@users.noreply.github.com>
2022-01-04 10:47:27 +0100

    laravel login (not final)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('/login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('/custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('/registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('/custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('/signout', [CustomAuthController::class, 'signOut'])->name('signout');
